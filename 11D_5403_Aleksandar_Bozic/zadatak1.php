9f1646f02dda983d3879d0e14efe98f509c5a369
Bozic991 <bozic.aleksandarsm@gmail.com>
2021-05-11 00:30:07 +0200

    domaci uradjen

---
+++ 11D_5403_Aleksandar_Bozic/zadatak1.php
@@ -0,0 +1,14 @@
+
+<?php
+    $promenljiva1 = 200;
+    $promenljiva2 = 80;
+    
+    echo $promenljiva1 + $promenljiva2;
+        echo "<br>";
+    echo $promenljiva1 - $promenljiva2;
+        echo "<br>";
+    echo $promenljiva1 * $promenljiva2;
+        echo "<br>";
+    echo $promenljiva1 / $promenljiva2;
+
+?>
---
+++ 11D_5403_Aleksandar_Bozic/zadatak2.php
@@ -0,0 +1,8 @@
+<?php
+    $a = 3;
+    $b = 5;
+    $c = 7;
+    echo "Zbir brojeva 3, 5 i 7 je ";
+    echo $a + $b + $c;
+
+?>
