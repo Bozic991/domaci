a69aefb76d7323db5ccc441565273e2ff4e5e881
Bozic991 <bozic.aleksandarsm@gmail.com>
2021-05-28 14:27:51 +0200

    21

---
+++ nedelja6/nedelja/21_5403_Aleksandar_Bozic/proizvod.php
@@ -0,0 +1,33 @@
+<style>
+    div{  
+        width:200px;
+        /* float: left; */
+        display: inline-block;
+    }
+    .crvena{
+        color:red;
+    }
+    .zelena{
+        color:green;
+    }
+    img{
+        height: 200px;
+        width:200px;
+    }
+</style>
+<?php
+    function pravi_div($ime, $slika, $cena, $popust){
+        echo "<div><img src=$slika>";
+        echo "<br>";
+        echo $ime;
+        if($popust>0){
+            $nova_cena=$cena -$popust;
+            echo "<p class=crvena>$cena</p>";
+            echo "<p class=zelena>$nova_cena</p";
+        }
+        elseif($popust==0){
+            echo "<p>$cena</p>";
+        }
+        echo "</div>";
+    }
+?>
---
+++ nedelja6/nedelja/21_5403_Aleksandar_Bozic/proizvodi.php
@@ -0,0 +1,23 @@
+<?php
+    include_once ("proizvod.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        pravi_div("Mercedes C klase", "c_klasa.jpg", 1000, 100);
+        echo "<br>";
+        pravi_div("Mercedes E klase", "e_klasa.jpg", 1500, 200);
+        echo "<br>";
+        pravi_div("Mercedes S klase", "s_klasa.jpg", 2000, 0);
+
+
+    ?>
+</body>
+</html>
